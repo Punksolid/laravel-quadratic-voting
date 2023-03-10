d4492b52cffb472c9ffbb203fac90a2040c743d2
Jose. Palazuelos <punksolid@gmail.com>
2023-03-10 16:34:35 -0700

    Basic Rector rules

--- rector.php
+++ rector.php
@@ -2,23 +2,69 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\ClassMethod\InlineArrayReturnAssignRector;
+use Rector\CodeQuality\Rector\ClassMethod\NarrowUnionTypeDocRector;
+use Rector\CodeQuality\Rector\Concat\JoinStringConcatRector;
+use Rector\CodeQuality\Rector\For_\ForRepeatedCountToOwnVariableRector;
+use Rector\CodeQuality\Rector\For_\ForToForeachRector;
+use Rector\CodeQuality\Rector\Foreach_\ForeachItemsAssignToEmptyArrayToAssignRector;
+use Rector\CodeQuality\Rector\Foreach_\ForeachToInArrayRector;
+use Rector\CodeQuality\Rector\FuncCall\ArrayKeysAndInArrayToArrayKeyExistsRector;
+use Rector\CodeQuality\Rector\FuncCall\BoolvalToTypeCastRector;
+use Rector\CodeQuality\Rector\FuncCall\CallUserFuncWithArrowFunctionToInlineRector;
+use Rector\CodeQuality\Rector\FuncCall\ChangeArrayPushToArrayAssignRector;
+use Rector\CodeQuality\Rector\FuncCall\FloatvalToTypeCastRector;
+use Rector\CodeQuality\Rector\FuncCall\IntvalToTypeCastRector;
+use Rector\CodeQuality\Rector\Identical\BooleanNotIdenticalToNotIdenticalRector;
+use Rector\CodeQuality\Rector\Identical\GetClassToInstanceOfRector;
+use Rector\CodeQuality\Rector\If_\CombineIfRector;
+use Rector\CodeQuality\Rector\If_\ConsecutiveNullCompareReturnsToNullCoalesceQueueRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
+use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
+use Rector\CodeQuality\Rector\NotEqual\CommonNotEqualRector;
+use Rector\CodeQuality\Rector\PropertyFetch\ExplicitMethodCallOverMagicGetSetRector;
+use Rector\CodeQuality\Rector\Ternary\ArrayKeyExistsTernaryThenValueToCoalescingRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
+use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\If_\ChangeOrIfReturnToEarlyReturnRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
 //        __DIR__ . '/config',
         __DIR__ . '/src',
-//        __DIR__ . '/tests',
+        __DIR__ . '/tests',
     ]);
 
     // register a single rule
-    $rectorConfig->rule(\Rector\PSR4\Composer\PSR4NamespaceMatcher::class);
+    $rectorConfig->rule(ChangeAndIfToEarlyReturnRector::class);
+    $rectorConfig->rule(ChangeOrIfReturnToEarlyReturnRector::class);
+    $rectorConfig->rule(AbsolutizeRequireAndIncludePathRector::class);
+    $rectorConfig->rule(ArrayKeyExistsTernaryThenValueToCoalescingRector::class);
+    $rectorConfig->rule(ArrayKeysAndInArrayToArrayKeyExistsRector::class);
+    $rectorConfig->rule(BooleanNotIdenticalToNotIdenticalRector::class);
+    $rectorConfig->rule(BoolvalToTypeCastRector::class);
+    $rectorConfig->rule(CallUserFuncWithArrowFunctionToInlineRector::class);
+    $rectorConfig->rule(ChangeArrayPushToArrayAssignRector::class);
+    $rectorConfig->rule(CombineIfRector::class);
+    $rectorConfig->rule(CommonNotEqualRector::class);
+    $rectorConfig->rule(CompleteDynamicPropertiesRector::class);
+    $rectorConfig->rule(ConsecutiveNullCompareReturnsToNullCoalesceQueueRector::class);
+    $rectorConfig->rule(ExplicitBoolCompareRector::class);
+    $rectorConfig->rule(ExplicitMethodCallOverMagicGetSetRector::class);
+    $rectorConfig->rule(FloatvalToTypeCastRector::class);
+    $rectorConfig->rule(ForRepeatedCountToOwnVariableRector::class);
+    $rectorConfig->rule(ForToForeachRector::class);
+    $rectorConfig->rule(ForeachItemsAssignToEmptyArrayToAssignRector::class);
+    $rectorConfig->rule(ForeachToInArrayRector::class);
+    $rectorConfig->rule(GetClassToInstanceOfRector::class);
+    $rectorConfig->rule(InlineArrayReturnAssignRector::class);
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rule(IntvalToTypeCastRector::class);
+    $rectorConfig->rule(JoinStringConcatRector::class);
+    $rectorConfig->rule(LogicalToBooleanRector::class);
+    $rectorConfig->rule(NarrowUnionTypeDocRector::class);
 
-    // define sets of rules
-//        $rectorConfig->sets([
-//            LevelSetList::UP_TO_PHP_81
-
-//        ]);
 };
